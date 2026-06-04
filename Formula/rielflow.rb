d902f77aa164a4c95f7c8773f3fe57087fc312ac
tacogips <me@tacogips.me>
2026-06-04 21:29:55 +0900

    chore: update rielflow formula to 0.1.9

--- Formula/rielflow.rb
+++ Formula/rielflow.rb
@@ -1,7 +1,7 @@
 class Rielflow < Formula
   desc "TypeScript/Bun workflow runtime for cooperative multi-agent execution"
   homepage "https://github.com/tacogips/rielflow"
-  version "0.1.8"
+  version "0.1.9"
   license "MIT"
 
   livecheck do
@@ -11,21 +11,21 @@ class Rielflow < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/tacogips/rielflow/releases/download/v0.1.8/rielflow-0.1.8-darwin-arm64.tar.gz"
-      sha256 "2e0e856c2452d6798d5864de57571591666513c62385192c748790771d469acd"
+      url "https://github.com/tacogips/rielflow/releases/download/v0.1.9/rielflow-0.1.9-darwin-arm64.tar.gz"
+      sha256 "9af40337551119fd83cf4acabf69737031e4f7320e549f9c7cb6bdf783082ef8"
     else
-      url "https://github.com/tacogips/rielflow/releases/download/v0.1.8/rielflow-0.1.8-darwin-x64.tar.gz"
-      sha256 "4898067cd7c88b60517f883fea72feffef2ff6b013c8fe96e4f4bdfb95b24556"
+      url "https://github.com/tacogips/rielflow/releases/download/v0.1.9/rielflow-0.1.9-darwin-x64.tar.gz"
+      sha256 "433324c3112defa4da5295b261c948dfc47ff87c2f6079468781fe384c040637"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/tacogips/rielflow/releases/download/v0.1.8/rielflow-0.1.8-linux-arm64.tar.gz"
-      sha256 "72cd92d54d0a8e56e7ee8287892a276b565cded756120ec2ee73c939fd5b6b73"
+      url "https://github.com/tacogips/rielflow/releases/download/v0.1.9/rielflow-0.1.9-linux-arm64.tar.gz"
+      sha256 "ae4f4617184434c1ed461b83fc26bd8d370461c86d4ea0cd9fe651b5c20a44af"
     else
-      url "https://github.com/tacogips/rielflow/releases/download/v0.1.8/rielflow-0.1.8-linux-x64.tar.gz"
-      sha256 "e046c657e9f66a3051b1d6c82dcf8e05727e57e6a8f5397599d3e6a51e1e6ad1"
+      url "https://github.com/tacogips/rielflow/releases/download/v0.1.9/rielflow-0.1.9-linux-x64.tar.gz"
+      sha256 "7dfd0e6d44de5d295a946ffd270b2521d9f5391add7c133945c23d4453677fad"
     end
   end
 
