4a931f7c5d7170dfd3626868c47d863792022d66
tacogips <me@tacogips.me>
2026-06-19 09:07:51 +0900

    Make Your Body cask audit clean

--- Casks/your-body.rb
+++ Casks/your-body.rb
@@ -4,9 +4,9 @@ cask "your-body" do
   version "0.1.0"
   sha256 "c82204258ed481f264275134a1d969a9fc9d1e25aab21de8ab77318029313506"
 
-  url "https://api.github.com/repos/tacogips/your-body/releases/assets/451688848",
-      verified: "github.com/tacogips/your-body/",
-      header: [
+  url "https://api.github.com/repos/tacogips/your-body/releases/assets/451688848?filename=YourBody_#{version}_aarch64.dmg",
+      verified: "api.github.com/repos/tacogips/your-body/releases/assets/",
+      header:   [
         "Accept: application/octet-stream",
         "Authorization: Bearer #{github_token}",
       ]
