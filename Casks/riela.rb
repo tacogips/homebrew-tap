8e3b181b41be38ab12195c4754076da6aa749259
tacogips <me@tacogips.me>
2026-06-25 17:35:03 +0900

    chore: update riela cask to 0.1.8

--- Casks/riela.rb
+++ Casks/riela.rb
@@ -1,11 +1,11 @@
 cask "riela" do
-  version "0.1.7"
+  version "0.1.8"
   arch arm: "darwin-arm64", intel: "darwin-x64"
 
-  sha256 arm: "c838e1703c9ec68d904e25276130b826777da1ad39e48e45e02b2104e314c4aa",
-         intel: "c240245bef56a728bf4b5e2244c9abe276345daf040d98baaac6b1d3a347ede9"
+  sha256 arm: "64dc3bc60cf8988a7af7ece17c83f80b7f416d56e1371edf2b461d3ec00392cf",
+         intel: "d24824c6378d07d59277e8f0d07204d9f798dee59bc41afc43b66b0a1b2cffd3"
 
-  url "https://github.com/tacogips/riela/releases/download/v0.1.7/riela-#{version}-#{arch}.dmg"
+  url "https://github.com/tacogips/riela/releases/download/v0.1.8/riela-#{version}-#{arch}.dmg"
   name "riela"
   desc "Swift-native workflow runtime with the macOS menu bar app and CLI"
   homepage "https://github.com/tacogips/riela"
