ae522c7803501e3b7ca8c462b15aca90eb7ca6ad
tacogips <me@tacogips.me>
2026-07-28 14:04:29 +0900

    fix: bypass cached 0.1.20 release assets

--- Formula/ccusage-gauge.rb
+++ Formula/ccusage-gauge.rb
@@ -11,10 +11,10 @@ class CcusageGauge < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/tacogips/ccusage-gauge/releases/download/v0.1.20/ccusage-gauge-0.1.20-darwin-arm64.tar.gz"
+      url "https://github.com/tacogips/ccusage-gauge/releases/download/v0.1.20/ccusage-gauge-0.1.20-darwin-arm64.tar.gz?revision=2"
       sha256 "1f86f2ee534392a9b7454e27807afd584d387d0cbcb83462321fc8cf367ddea9"
     else
-      url "https://github.com/tacogips/ccusage-gauge/releases/download/v0.1.20/ccusage-gauge-0.1.20-darwin-x64.tar.gz"
+      url "https://github.com/tacogips/ccusage-gauge/releases/download/v0.1.20/ccusage-gauge-0.1.20-darwin-x64.tar.gz?revision=2"
       sha256 "f668cb4d75d7c83314b43be74da44153122c1781f279faec5443b34a65228fdb"
     end
   end
