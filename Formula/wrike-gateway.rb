d6066fc25bf7dfe98eec0dca42c9ed525f55ecd4
tacogips <me@tacogips.me>
2026-08-06 18:20:28 +0900

    wrike-gateway 0.2.1

--- Formula/wrike-gateway.rb
+++ Formula/wrike-gateway.rb
@@ -1,7 +1,7 @@
 class WrikeGateway < Formula
   desc "Capability-scoped Wrike API v4 CLI with a project-owned GraphQL contract"
   homepage "https://github.com/tacogips/wrike-gateway"
-  version "0.2.0"
+  version "0.2.1"
   license "MIT"
 
   livecheck do
@@ -11,11 +11,11 @@ class WrikeGateway < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/tacogips/wrike-gateway/releases/download/v0.2.0/wrike-gateway-0.2.0-darwin-arm64.tar.gz"
-      sha256 "6ee6b84945234b1447c0665e3d85c536f669d4ba24f1aac510658ef16837e33b"
+      url "https://github.com/tacogips/wrike-gateway/releases/download/v0.2.1/wrike-gateway-0.2.1-darwin-arm64.tar.gz"
+      sha256 "42f547f07d1584925fa756018ecb05c033a41849e6b20bb950d1e98dc498bb87"
     else
-      url "https://github.com/tacogips/wrike-gateway/releases/download/v0.2.0/wrike-gateway-0.2.0-darwin-x64.tar.gz"
-      sha256 "2ac8140e5e13f7fd52ce66328f80fc5fdb8f43886d45704c8003d02b47b8120c"
+      url "https://github.com/tacogips/wrike-gateway/releases/download/v0.2.1/wrike-gateway-0.2.1-darwin-x64.tar.gz"
+      sha256 "1a5b2867ac83c237301a2ca8ce5b9ccb11b51b3fbab3356cae53fe0f4a190ba8"
     end
   end
 
@@ -26,6 +26,6 @@ class WrikeGateway < Formula
   end
 
   test do
-    assert_match "0.2.0", shell_output("#{bin}/wrike-gateway-reader --version")
+    assert_match "0.2.1", shell_output("#{bin}/wrike-gateway-reader --version")
   end
 end
