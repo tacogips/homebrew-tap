4984d43660ded677877c09d9f14277fb82b30464
tacogips <me@tacogips.me>
2026-08-07 15:02:59 +0900

    chore: release kinko 0.1.9

--- Formula/kinko.rb
+++ Formula/kinko.rb
@@ -10,21 +10,21 @@ class Kinko < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/tacogips/kinko/releases/download/v0.1.8/kinko_0.1.8_darwin_arm64.tar.gz"
-      sha256 "82793aae92dee6dc6e4c9b032eeaf1331e1067b592ed0a209eaa888ada513f58"
+      url "https://github.com/tacogips/kinko/releases/download/v0.1.9/kinko_0.1.9_darwin_arm64.tar.gz"
+      sha256 "171066fe4f035efb6c81cd9f98be4db59ced2d165ed3e4d598cd805c57a0de05"
     else
-      url "https://github.com/tacogips/kinko/releases/download/v0.1.8/kinko_0.1.8_darwin_amd64.tar.gz"
-      sha256 "47967665e45b38aeb9da624af6ca767f8ee9b9267b22282b94d7c933dfa924ee"
+      url "https://github.com/tacogips/kinko/releases/download/v0.1.9/kinko_0.1.9_darwin_amd64.tar.gz"
+      sha256 "a4ac42c209255229d02dd14b84cfd18fa0ab3dd134376a41b28728be6d6616aa"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/tacogips/kinko/releases/download/v0.1.8/kinko_0.1.8_linux_arm64.tar.gz"
-      sha256 "44bc22aedbc1c325fe5eac300965ec695c325582815884ab0190b0498133c954"
+      url "https://github.com/tacogips/kinko/releases/download/v0.1.9/kinko_0.1.9_linux_arm64.tar.gz"
+      sha256 "eedeccd8d991fcbdd30de0a74159b9f66ad88666794f71310c2bdcb5c910d4ae"
     else
-      url "https://github.com/tacogips/kinko/releases/download/v0.1.8/kinko_0.1.8_linux_amd64.tar.gz"
-      sha256 "bd4b436bc0a3a31fca34dd994b5ad0f725d718361d21775f370fb675d403533f"
+      url "https://github.com/tacogips/kinko/releases/download/v0.1.9/kinko_0.1.9_linux_amd64.tar.gz"
+      sha256 "9386aa86c2662a2f1ba58784d2fccf681af3758eead3228d27e926e20229384b"
     end
   end
 
@@ -33,6 +33,6 @@ class Kinko < Formula
   end
 
   test do
-    assert_equal "0.1.8", shell_output("#{bin}/kinko version").strip
+    assert_equal "0.1.9", shell_output("#{bin}/kinko version").strip
   end
 end
