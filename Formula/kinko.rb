8434ac631bec7d1312f01b35997bad1694960a1c
tacogips <me@tacogips.me>
2026-08-10 19:06:35 +0900

    chore: release kinko 0.1.10
    Update Darwin and Linux release URLs and checksums for both supported architectures, and align the formula test with kinko v0.1.10.

--- Formula/kinko.rb
+++ Formula/kinko.rb
@@ -10,21 +10,21 @@ class Kinko < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/tacogips/kinko/releases/download/v0.1.9/kinko_0.1.9_darwin_arm64.tar.gz"
-      sha256 "171066fe4f035efb6c81cd9f98be4db59ced2d165ed3e4d598cd805c57a0de05"
+      url "https://github.com/tacogips/kinko/releases/download/v0.1.10/kinko_0.1.10_darwin_arm64.tar.gz"
+      sha256 "46a9a05bf3ab76cd6f9fb950b3aa7a2a7c8e4e2f262fdd5a1aae3b9904d9f73f"
     else
-      url "https://github.com/tacogips/kinko/releases/download/v0.1.9/kinko_0.1.9_darwin_amd64.tar.gz"
-      sha256 "a4ac42c209255229d02dd14b84cfd18fa0ab3dd134376a41b28728be6d6616aa"
+      url "https://github.com/tacogips/kinko/releases/download/v0.1.10/kinko_0.1.10_darwin_amd64.tar.gz"
+      sha256 "ba1262ae25bc4e16e60dad0372d8cb38ccce76a8e523702ebe9e0263842823ce"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/tacogips/kinko/releases/download/v0.1.9/kinko_0.1.9_linux_arm64.tar.gz"
-      sha256 "eedeccd8d991fcbdd30de0a74159b9f66ad88666794f71310c2bdcb5c910d4ae"
+      url "https://github.com/tacogips/kinko/releases/download/v0.1.10/kinko_0.1.10_linux_arm64.tar.gz"
+      sha256 "0ca7be71a7f714762d6e1323bf97790faa4a87ef297c2d1ff8764e4f4b785177"
     else
-      url "https://github.com/tacogips/kinko/releases/download/v0.1.9/kinko_0.1.9_linux_amd64.tar.gz"
-      sha256 "9386aa86c2662a2f1ba58784d2fccf681af3758eead3228d27e926e20229384b"
+      url "https://github.com/tacogips/kinko/releases/download/v0.1.10/kinko_0.1.10_linux_amd64.tar.gz"
+      sha256 "5054753a8254748db761cd689f07b964e10509ed7d67bf52e261d6f08f126281"
     end
   end
 
@@ -33,6 +33,6 @@ class Kinko < Formula
   end
 
   test do
-    assert_equal "0.1.9", shell_output("#{bin}/kinko version").strip
+    assert_equal "0.1.10", shell_output("#{bin}/kinko version").strip
   end
 end
