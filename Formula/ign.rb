8a95e28cb3a0ade3d0413d7b90e27fcbe66c510d
tacogips <me@tacogips.me>
2026-08-11 22:41:19 +0900

    fix: report ign 0.1.23 in API metadata

--- Formula/ign.rb
+++ Formula/ign.rb
@@ -1,6 +1,7 @@
 class Ign < Formula
   desc "Template-based code generation CLI tool"
   homepage "https://github.com/tacogips/ign"
+  version "0.1.23"
   license "MIT"
 
   livecheck do
@@ -10,20 +11,20 @@ class Ign < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/tacogips/ign/releases/download/v0.1.23/ign-0.1.23-darwin-arm64.tar.gz"
+      url "https://github.com/tacogips/ign/releases/download/v#{version}/ign-#{version}-darwin-arm64.tar.gz"
       sha256 "43efaceb9cebd8d012fe3ba7ab242173c7187927903180890981d325bca136fb"
     else
-      url "https://github.com/tacogips/ign/releases/download/v0.1.23/ign-0.1.23-darwin-x64.tar.gz"
+      url "https://github.com/tacogips/ign/releases/download/v#{version}/ign-#{version}-darwin-x64.tar.gz"
       sha256 "389899bc321c7eaf0727120415a979c55f5c846969f0ffb8132c44ba9f4d1429"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/tacogips/ign/releases/download/v0.1.23/ign-0.1.23-linux-arm64.tar.gz"
+      url "https://github.com/tacogips/ign/releases/download/v#{version}/ign-#{version}-linux-arm64.tar.gz"
       sha256 "18fe91127c5b4ad6f7c15dd997506b819462d99b69f3f09b249b6f4809417eb7"
     else
-      url "https://github.com/tacogips/ign/releases/download/v0.1.23/ign-0.1.23-linux-x64.tar.gz"
+      url "https://github.com/tacogips/ign/releases/download/v#{version}/ign-#{version}-linux-x64.tar.gz"
       sha256 "ec906bb822002fc3c11a4c3d8c9f36fdb405ceb7d6cfbfeebe982c8751e6d8f7"
     end
   end
