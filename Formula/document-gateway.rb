c28c973892b908355430509623f2c0ebb8571413
tacogips <me@tacogips.me>
2026-08-14 01:04:40 +0900

    feat: add document-gateway 0.1.0 formula
    Install the complete set of Google Docs, Sheets, and Drive gateway executables from checksum-pinned macOS arm64 and x86_64 release archives.

---
+++ Formula/document-gateway.rb
@@ -0,0 +1,29 @@
+class DocumentGateway < Formula
+  desc "Least-privilege Google Docs, Sheets, and Drive CLI gateways"
+  homepage "https://github.com/tacogips/document-gateway"
+  version "0.1.0"
+  license "MIT"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/tacogips/document-gateway/releases/download/v0.1.0/document-gateway-0.1.0-darwin-arm64.tar.gz"
+      sha256 "133acedebb0145d414d49d823ded7c4a32c16c56778224c961252fc3074ccaf7"
+    else
+      url "https://github.com/tacogips/document-gateway/releases/download/v0.1.0/document-gateway-0.1.0-darwin-x64.tar.gz"
+      sha256 "f98df4ee6f015e92bbd5a83af66a89170ebab422cd1853586d983ede1c4caa47"
+    end
+  end
+
+  def install
+    bin.install Dir["bin/*"]
+  end
+
+  test do
+    assert_match "0.1.0", shell_output("#{bin}/document-gateway --version")
+  end
+end
