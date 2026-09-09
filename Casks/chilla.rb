023aa70f31904eee235310ca322805fbcef8b173
tacogips <me@tacogips.me>
2026-09-10 00:07:05 +0900

    chore: update chilla cask to 0.3.0
    Update the chilla cask to the notarized v0.3.0 GitHub DMG and its verified published-asset SHA-256 checksum.

--- Casks/chilla.rb
+++ Casks/chilla.rb
@@ -1,6 +1,6 @@
 cask "chilla" do
-  version "0.2.0"
-  sha256 "585c4f571f37159a392213d11a5dfe16a4d4f5ce7f83a509323c0b8a76f49eb3"
+  version "0.3.0"
+  sha256 "9b5b41e8cabb077060b8ad4cf4a58a7b0bab8d80362e5e2a344172f7c9e23313"
 
   url "https://github.com/tacogips/chilla/releases/download/v#{version}/chilla_#{version}_aarch64.dmg"
   name "chilla"
