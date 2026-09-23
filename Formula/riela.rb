e1e6ad95bf00f03f74c54974be8ed2b717b2ec7d
tacogips <me@tacogips.me>
2026-09-23 14:42:21 +0900

    chore: update riela formula to 0.1.48

--- Formula/riela.rb
+++ Formula/riela.rb
@@ -10,8 +10,8 @@ class Riela < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/tacogips/riela/releases/download/v0.1.47/riela-0.1.47-darwin-arm64.tar.gz"
-      sha256 "5016cb43d90ef6cfc93a56394b5e62938072b0c5890664392c4c153cc68ed5ba"
+      url "https://github.com/tacogips/riela/releases/download/v0.1.48/riela-0.1.48-darwin-arm64.tar.gz"
+      sha256 "c5388105e98e0de9ef3ea762e9ac4519a8aefeb3f785d4926bb96e8a335266bc"
     end
   end
 
