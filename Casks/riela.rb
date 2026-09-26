5c29a9c293abfe3cbd7bc0476a7c4849c1a9282b
tacogips <me@tacogips.me>
2026-09-26 18:53:24 +0900

    chore: publish riela 0.2.1 ARM64 cask

--- Casks/riela.rb
+++ Casks/riela.rb
@@ -1,10 +1,10 @@
 cask "riela" do
   arch arm: "darwin-arm64"
 
-  version "0.2.0"
-  sha256 "fc8941d8a8daf385976ddc3caf3af57d472812b6d6b407f8127a634c8c5cf1ad"
+  version "0.2.1"
+  sha256 "ad0a350f473461cacb095d0ca93f43844887d7d3c96ae5f48fe8e0f9821f0853"
 
-  url "https://github.com/tacogips/riela/releases/download/v0.2.0/riela-#{version}-#{arch}.dmg"
+  url "https://github.com/tacogips/riela/releases/download/v0.2.1/riela-#{version}-#{arch}.dmg"
   name "riela"
   desc "Swift-native workflow runtime with a menu bar app and CLI"
   homepage "https://github.com/tacogips/riela"
